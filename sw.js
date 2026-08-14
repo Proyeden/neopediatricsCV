d81a30a32c5327aae3a2336b941a6b707b0af3a7
Proyeden <proyectosdeniscp@gmail.com>
2026-08-14 10:34:17 -0600

    Update service worker version to 2026-08-13b

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
    Al publicar una versión nueva del archivo, suba VERSION: eso descarta
    la caché anterior y todos reciben la nueva sin borrar nada a mano.
    ============================================================================ */
-const VERSION = "2026-08-13";
+const VERSION = "2026-08-13b";
 const ESENCIALES = [
   "./",
   "./index.html",
